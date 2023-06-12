cb93896f49e14c5bb62576741070b127b5c7c6a2
Eividas Sakalys <eividas.sakalys@vinted.com>
2023-06-12 12:47:33 +0300

    Second first commit

---
+++ Section 2 - Ruby/lesson_1_basics.rb
@@ -0,0 +1,26 @@
+
+# Hashtag for comments
+
+
+# Printing to console
+puts 'Hello World'
+p 'Hello World'
+print "Hello World\n"
+puts 'Hello hello'
+
+# Variables
+hi = 'Hello World'
+puts hi
+
+# Functions
+def say_hi
+  puts 'Hi'
+end
+
+say_hi
+
+def say_hello(name)
+  puts "hello #{name}"
+end
+
+say_hello 'Prison Mike' 
